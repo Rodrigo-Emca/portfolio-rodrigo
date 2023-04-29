8eee896cb58b14f21f3ad7e28a6d77f96d3b6727
Rodrigo <contactorodrigocabrera@gmail.com>
2023-04-29 19:14:21 -0300

    Se agregan los proyectos y los enlaces

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -3,6 +3,7 @@ import githubIcon from "../../assets/github-icon.svg"
 import externalLinkIcon from "../../assets/external-link-icon.svg"
 import ScrollAnimation from "react-animate-on-scroll";
 
+import matearImage from '../../assets/matear-imagen.jpg'
 
 export function Portfolio(){
   return(
@@ -16,22 +17,25 @@ export function Portfolio(){
           <header>
           <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
           <div className="project-links">
-            <a href="https://github.com/joaotuliojt/fc-squad43" target="_blank" rel="noreferrer">
+            <a href="https://github.com/Rodrigo-Emca/matear-front" target="_blank" rel="noreferrer">
               <img src={githubIcon} alt="GitHub" />
             </a>
+            <a href="https://mate-ar.vercel.app/home" target="_blank" rel="noreferrer">
+                <img src={externalLinkIcon} alt="Visitar site" />
+              </a>
           </div>
           </header>
           <div className="body">
-            <h3>FCallendar</h3>
-            <p>Uma aplicação Web realizada durante o Hackathon do programa de formação da <a href="https://www.fcamara.com.br/"> FCamara</a>. Esta aplicação serve para ajudar na volta do trabalho remoto para o presencial.
+            <h3>MateAr</h3>
+            <p>Una aplicación web e-Commerce creada para un emprendimiento local. <a href="https://mate-ar.vercel.app/home"> MateAr</a> permite registrarse e iniciar sesión para comprar y pagar con MercadoPago.
             </p>
           </div>
           <footer>
             <ul className="tech-list">
+              <li>React</li>
               <li>Node</li>
               <li>Express</li>
-              <li>MySql</li>
-              <li>Ejs</li>
+              <li>MongoDB</li>
             </ul>
           </footer>
         </div>
@@ -42,24 +46,25 @@ export function Portfolio(){
             <header>
             <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
             <div className="project-links">
-              <a href="https://github.com/joaotuliojt/github-search" target="_blank" rel="noreferrer">
+              <a href="https://github.com/Rodrigo-Emca/Minga" target="_blank" rel="noreferrer">
                 <img src={githubIcon} alt="GitHub" />
               </a>
-              <a href="https://github-search-blond.vercel.app/" target="_blank" rel="noreferrer">
+              <a href="https://minga-nine.vercel.app/" target="_blank" rel="noreferrer">
                 <img src={externalLinkIcon} alt="Visitar site" />
               </a>
             </div>
             </header>
             <div className="body">
-              <h3>Github Searcher</h3>
-              <p>Uma versão mais agradável para ver seu perfil do GitHub, inclui visualização de dados , repositórios, compartilhamento entre outros.
-              </p>
+              <h3>Minga</h3>
+              <p>Una website para leer mangas. Pueden registrarse usuarios (para lectura) y editoriales (para publicar, editar y eliminar sus mangas). Incorpora MercadoPago para realizar donaciones.</p>
             </div>
             <footer>
               <ul className="tech-list">
                 <li>React</li>
-                <li>Typescript</li>
-                <li>Styled Components</li>
+                <li>Redux</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MongoDB</li>
               </ul>
             </footer>
           </div>
@@ -70,20 +75,23 @@ export function Portfolio(){
             <header>
             <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
             <div className="project-links">
-              <a href="https://joaotuliojt.github.io/sunnyside-agency/" target="_blank" rel="noreferrer">
+              <a href="https://github.com/Rodrigo-Emca/Happy-Tails---Petshop" target="_blank" rel="noreferrer">
+                <img src={githubIcon} alt="GitHub" />
+              </a>
+              <a href="https://rodrigo-emca.github.io/Happy-Tails---Petshop/index.html" target="_blank" rel="noreferrer">
                 <img src={externalLinkIcon} alt="Visitar site" />
               </a>
             </div>
             </header>
             <div className="body">
-              <h3>Sunnyside agency</h3>
-              <p>Uma lading page de uma empresa fictícia proposta pelo <a href="https://www.frontendmentor.io/">Frontend Mentor.</a>
-              </p>
+              <h3>Happy Tails</h3>
+              <p>Un PetShop creado para un challenge de 72hs, realizada en forma cooperativa. Contiene un filtro por texto y un Shop para agregar/quitar productos.</p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>Html</li>
-                <li>Css</li>
+                <li>HTML5</li>
+                <li>CSS3</li>
+                <li>Bootstrap</li>
                 <li>JavaScript</li>
               </ul>
             </footer>
@@ -95,44 +103,20 @@ export function Portfolio(){
             <header>
             <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
             <div className="project-links">
-              <a href="https://joaotulio.vercel.app/" target="_blank" rel="noreferrer">
-                <img src={externalLinkIcon} alt="Visitar site" />
-              </a>
-            </div>
-            </header>
-            <div className="body">
-              <h3>Portfólio V1</h3>
-              <p>Uma versão alternativa de meu portfólio, utilizando VueJs.
-              </p>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>VueJs</li>
-                <li>JavaScript</li>
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
-
-        <ScrollAnimation animateIn="flipInX">
-          <div className="project">
-            <header>
-            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-            <div className="project-links">
-              <a href="https://github.com/joaotuliojt/dtmoney-trilha-reactjs" target="_blank" rel="noreferrer">
+              <a href="https://github.com/Rodrigo-Emca/matear_mobile" target="_blank" rel="noreferrer">
                 <img src={githubIcon} alt="Visitar site" />
               </a>
             </div>
             </header>
             <div className="body">
-              <h3>Dtmoney</h3>
-              <p>Um gerenciador de finanças para analisar seus ganhos, gastos e ter um controle maior de suas finanças.
-              </p>
+              <h3>MateAr Mobile</h3>
+              <p>App Mobile de MateAr (e-Commerce) creada con React Native. Permite al usuario registrarse, iniciar sesión, ver productos, sumarlos a un shop y finalizar su compra, usando MercadoPago.</p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>VueJs</li>
-                <li>JavaScript</li>
+                <li>React Native</li>
+                <li>Redux</li>
+                <li>MongoDB</li>
               </ul>
             </footer>
           </div>
@@ -143,30 +127,51 @@ export function Portfolio(){
             <header>
             <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
             <div className="project-links">
-              <a href="https://github.com/joaotuliojt/dv-musica-library-page" target="_blank" rel="noreferrer">
+              <a href="https://github.com/Rodrigo-Emca/matear_mobile" target="_blank" rel="noreferrer">
                 <img src={githubIcon} alt="Visitar site" />
-              </a>
-              <a href="https://joaotuliojt.github.io/dv-musica-library-page/" target="_blank" rel="noreferrer">
-                <img src={externalLinkIcon} alt="Visitar site" />
               </a>
             </div>
             </header>
             <div className="body">
-              <h3>Music Library</h3>
-              <p>Uma interface para uma biblioteca de músicas, semelhante ao Spotify.
-              </p>
+              <h3>Minga Mobile</h3>
+              <p>App Mobile de Minga construida con React Native. El usuario puede iniciar sesión para leer mangas y comics.</p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>Html</li>
-                <li>Css</li>
-                <li>JavaSript</li>
+                <li>React Native</li>
+                <li>Redux</li>
+                <li>MongoDB</li>
               </ul>
             </footer>
           </div>
         </ScrollAnimation>
 
-
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+            <div className="project-links">
+              <a href="https://github.com/Rodrigo-Emca/amazing_events" target="_blank" rel="noreferrer">
+                <img src={githubIcon} alt="GitHub" />
+              </a>
+              <a href="https://rodrigo-emca.github.io/amazing_events/index.html" target="_blank" rel="noreferrer">
+                <img src={externalLinkIcon} alt="Visitar site" />
+              </a>
+            </div>
+            </header>
+            <div className="body">
+              <h3>Amazing Events</h3>
+              <p>Una website que consume una API para obtener diferentes eventos. Contiene filtros por categorias y texto. </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>HTML5</li>
+                <li>CSS3</li>
+                <li>JavaScript</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
 
       </div>
     </Container>
